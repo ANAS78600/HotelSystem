ff35dc936647517f7de89eb734f742f0657c8741
p2588312 <P2588312@my365.dmu.ac.uk>
2022-03-05 14:05:38 +0000

    Added testing for staff class

---
+++ HotelTesting/tstStaff.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace HotelTesting
+{
+    [TestClass]
+    public class tstStaff
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            //create an instance of the class we want to create
+            clsStaff AStaff = new clsStaff();
+            //test to see that it exists
+            Assert.IsNotNull(AStaff);
+        }
+    }
+
+    class clsStaff
+    {
+    }
+}

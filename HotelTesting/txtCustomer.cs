ff26c3aa4daae79a04b73ad1bb48c146641d4994
p2575125 <p2575125@my365.dmu.ac.uk>
2022-03-09 11:25:50 +0000

    Added txtCustomer Class

---
+++ HotelTesting/txtCustomer.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace HotelTesting
+{
+    [TestClass]
+    public class txtCustomer
+    {
+        [TestMethod]
+        public void InstanceOk()
+        {
+            //create an instance of the class we want to  create 
+            clsCustomer AnCustomer = new clsCustomer();
+            Assert.IsNotNull(AnCustomer);
+        }
+    }
+
+    class clsCustomer
+    {
+        public clsCustomer()
+        {
+        }
+    }
+}
